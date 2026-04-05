6e3ae94f023653d6f1913ed90d6824b8c14aec23
Yash Vandra <yashvandra10@gmail.com>
2026-04-06 01:48:18 +0530

    [REMOVE] removed the old addons

--- valance_internationals_addons/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import stock_picking
-from . import sale_order
-from . import sale_order_line
-from . import product_template
-from . import product_product
-from . import account_move
-from . import account_move_line
