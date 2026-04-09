da51c5e3e251b1bd5d2616df0a8bd58448728062
Yash Vandra <yashvandra10@gmail.com>
2026-04-10 01:24:55 +0530

    [Add] added a new storage location field

--- valance_internationals_addons/models/product_template.py
+++ valance_internationals_addons/models/product_template.py
@@ -6,8 +6,26 @@ class ProductTemplate(models.Model):
 
     lr_number = fields.Char(string="LR Number")
     part_number = fields.Char(string="Part Number")
-
+    
     _part_number_uniq = models.Constraint(
         'unique(part_number)',
         'Part Number must be unique! A product with this Part Number already exists.',
     )
+   
+    storage_location_id = fields.Many2one(
+        'stock.location',
+        string='Storage Location',
+        compute='_compute_storage_location',
+        store=True,
+    )
+
+    @api.depends('product_variant_ids.stock_quant_ids.location_id','product_variant_ids.stock_quant_ids.quantity')
+    def _compute_storage_location(self):
+        for product in self:
+            quant = self.env['stock.quant'].search([
+                ('product_id.product_tmpl_id', '=', product.id),
+                ('location_id.usage', '=', 'internal'),
+                ('quantity', '>', 0)
+            ], limit=1, order='quantity desc')
+            
+            product.storage_location_id = quant.location_id if quant else False
